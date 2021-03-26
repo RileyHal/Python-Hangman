8c8f2a463175ef2c4881a15ca8713c829bc42253
RileyHal <halbertriley@gmail.com>
2021-03-26 00:39:50 -0400

    Added game function
    Added the main code for the game, still needs some things to be added and bugs to be worked out.

--- hangman.py
+++ hangman.py
@@ -49,7 +49,8 @@ HANGMANPICS = ['''
  /|\  |
  / \  |
       |
-=========''']
+=========
+YOU LOST!''']
 
 #Word bank of animals
 words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
@@ -60,7 +61,8 @@ words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
          'stork swan tiger toad trout turkey turtle weasel whale wolf '
          'wombat zebra ').split()
 
-def lettersInWord(word):
+#returns an array of letters contained in a word
+def getLetters(word):
     wordContains = []
     for letter in word:
         if letter in wordContains:
@@ -68,5 +70,56 @@ def lettersInWord(word):
         wordContains.append(letter)
     return wordContains
 
-letters = lettersInWord(words[random.randint(0,len(words))])
+#display correctly guessed letters and underscores
+def displayWordStatus(word,lettersGuessed):
+    finalString = ""
+    for x in range(len(word)):
+        for letter in lettersGuessed:
+            if letter == word[x]:
+                finalString.append(letter)
+            else:
+                finalString.append("_")
+    return finalString
+
+#return true if player wants to play again and false if not
+def Game():
+    selectedWord = words[random.randint(0,len(words))]
+    lettersInWord = getLetters(selectedWord)
+    lettersGuessed = []
+    incorrectGuesses = 0
+    gameStatus = True
+
+    while gameStatus:
+        letterGuessed = ""
+        print(HANGMANPICS[incorrectGuesses])
+        print(displayWordStatus(selectedWord,letterGuessed))
+
+        letterGuessed = input("Enter a letter > ").lower()
+
+
+        #check correct guess
+        if letterGuessed in lettersInWord:
+            lettersGuessed.append(letterGuessed)
+            continue
+        #check if letter has already been guessed
+        elif letterGuessed in lettersGuessed:
+            print("letter has already been guessed!")
+            continue
+        #if none of these then it was a wrong guess
+        else:
+            lettersGuessed.append(letterGuessed)
+            incorrectGuesses += 1
+
+        #check if game over
+        if incorrectGuesses == 6:
+            gameStatus = False
+    
+    #ask to play again
+    response = input("Would you like to play again? (y/n) > ")
+    if response == y:
+        return True
+    return False
+
+Game()
+
 
